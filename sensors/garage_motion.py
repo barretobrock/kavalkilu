d7ae227a1cb731af3d8b8827b0647119c6675cf4
barretobrock <barret.obrock@gmail.com>
2018-07-07 20:41:29 -0500

    KILU-9: Include logging in motion sensing

--- sensors/garage_motion.py
+++ sensors/garage_motion.py
@@ -4,7 +4,9 @@
 from kavalkilu.tools.sensors import PIRSensor
 from kavalkilu.tools.light import HueBulb
 from kavalkilu.tools.openhab import OpenHab
+from kavalkilu.tools.log import Log
 from datetime import datetime
+import os
 
 
 MOTION_PIN = 18
@@ -12,6 +14,8 @@ lights = ['Garage 1', 'Garage 2']
 
 # Set up OpenHab connection
 oh = OpenHab()
+# Initiate Log
+log = Log('garage_motion', os.path.abspath('/home/pi/logs'), 'motion')
 
 # Set up motion detector
 md = PIRSensor(MOTION_PIN)
@@ -27,9 +31,12 @@ if tripped:
             light.turn_on()
             # Log motion
             req = oh.update_value('Motion_Garaaz_PIR', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            log.debug('Motion detected at {:%Y-%m-%d %H:%M:%S}'.format(datetime.now()))
 else:
     # Turn off the light if it's been on for the past 5 min cycle without any trips
+    log.debug('No motion detected for this period.')
     for light in light_list:
         if light.get_status():
             light.turn_off()
+            log.debug('Lights were on. Turned off')
 
