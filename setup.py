8cb84e50bebd95c43adcd1ffe0d869fd3315e21a
barretobrock <barret.obrock@gmail.com>
2018-12-08 16:32:21 -0600

    KILU-12: Adapt versioning, more adjustment to gitignore

--- setup.py
+++ setup.py
@@ -8,25 +8,10 @@ import codecs
 import re
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-
-def read(*parts):
-    with codecs.open(os.path.join(here, *parts), 'r') as fp:
-        return fp.read()
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
 
 setup_args = {
     'name': 'kavalkilu',
-    'version': find_version("package", "__init__.py"),
+    'version': '0.2.2',
     'license': 'MIT',
     'description': 'A    Library for Integrating Home Automation Components',
     'url': 'https://github.com/barretobrock/kavalkilu',
