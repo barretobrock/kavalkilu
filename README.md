043ce4c00b82f58f5bca422a71a10a4cc04f8cdf
barretobrock <barret.obrock@gmail.com>
2018-12-09 13:55:14 -0600

    KILU-14: Update commute script and README.

--- README.md
+++ README.md
@@ -2,9 +2,21 @@
 Some updated scripts for personal use throughout various personal projects (mostly Raspberry Pi-centric)
 
 ## Installation
-```
+```bash
 pip3 install git+https://github.com/barretobrock/kavalkilu.git  
 ```
+
+## Update
+```bash
+pip3 install git+https://github.com/barretobrock/kavalkilu.git --upgrade
+```
+
+## Troubleshooting
+ - First time building a proper package, so there's a lot that might go wrong. 
+    - Currently, there's an issue with the package not updating when using `pip`.
+        To bypass this, I copy the contents of the package directory to the 
+        package location in `site-packages`:
+        `cp -r ~/kavalkilu ~/.local/lib/python3.6/site-packages/kavalkilu`
 
 ## Tips
 
--- sensors/commute/commute_calc.py
+++ sensors/commute/commute_calc.py
@@ -1,23 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import inspect
 import os
-import sys
-import socket
-# set system paths for importing custom modules/functions
-cur_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-hostname = socket.gethostname()
-if '__file__' in globals():
-    # if not running in debugger, assign parent directory of file to system paths
-    parent_dir = os.path.dirname(os.path.dirname(cur_dir))
-    sys.path.insert(0, os.path.join(parent_dir, 'blue2'))
-else:
-    # otherwise set system paths based on project directory in PyCharm
-    sys.path.insert(0, os.path.join(cur_dir, 'blue2'))
-# import custom modules
-from primary.maintools import Paths
-from comm.commtools import PBullet
-from logger.pylogger import Log
+from kavalkilu.tools import Paths, Log
+from kavalkilu.tools.message import PBullet
 import gspread
 from collections import OrderedDict
 from oauth2client.service_account import ServiceAccountCredentials
