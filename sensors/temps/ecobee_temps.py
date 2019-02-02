85a70b523da3dff030a203afc532c15333c0fd08
barretobrock <barret.obrock@gmail.com>
2019-02-02 11:45:39 -0600

    KILU-18: Update first temp collection script to more structured procedure

--- sensors/temps/ecobee_temps.py
+++ sensors/temps/ecobee_temps.py
@@ -14,6 +14,46 @@ VAL_TBLS = ['temps', 'humidity']
 log = Log('ecobee_temp', 'temp', log_lvl='INFO')
 log.debug('Logging initiated')
 
+
+def log_values(loc_id, ts, measurement_list, tbl_list, conn, log=log):
+    """
+    Logs measurements to the database. If measurements are None-type, fails gracefully.
+    Args:
+        loc_id: int, the location id
+        ts: str, ISO-format timestamp
+        measurement_list: list of floats, the measurements to record
+        tbl_list: list of str, the tables to insert into. NOTE: Must be of equal length to measurement_list
+        conn: database connection
+        log: logger
+    """
+
+    if len(measurement_list) != len(tbl_list):
+        raise ValueError("List size mismatch: measurement_list and tbl_list sizes aren't equal!")
+
+    # Build a dictionary of the values we're moving around
+    insert_list = [
+        {
+            'loc_id': loc_id,
+            'record_date': ts,
+            'record_value': x,
+            'tbl': y
+        } for x, y in zip(measurement_list, tbl_list)
+    ]
+
+    for d in insert_list:
+        if d['record_value'] is None:
+            # If no record value, don't go through with inserting into table
+            log.error('No record value found. Not recording to table "{tbl}"'.format(**d))
+        else:
+            # For humidity and temp, insert into tables
+            insert_query = """
+                INSERT INTO {tbl} (`loc_id`, `record_date`, `record_value`)
+                VALUES ({loc_id}, "{record_date}", {record_value})
+            """.format(**d)
+            insert_log = conn.execute(insert_query)
+            log.debug('Query sent to table "{tbl}". Result shows {0} rows affected.'.format(insert_log.rowcount, **d))
+
+
 temp_dict = oh.read_value('Temp_Upstairs')
 hum_dict = oh.read_value('Hum_Upstairs')
 
@@ -24,17 +64,8 @@ temp_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 temp_avg = round(float(temp_dict['state']), 2)
 hum_avg = round(float(hum_dict['state']), 2)
 
-# Build a dictionary of the values we're moving around
-vals = [{'loc_id': LOC_ID, 'record_date': temp_time,
-         'record_value': x, 'tbl': y} for x, y in zip([temp_avg, hum_avg], VAL_TBLS)]
-
-for val_dict in vals:
-    # For humidity and temp, insert into tables
-    insert_query = """
-        INSERT INTO {tbl} (`loc_id`, `record_date`, `record_value`)
-        VALUES ({loc_id}, "{record_date}", {record_value})
-    """.format(**val_dict)
-    insert_log = conn.execute(insert_query)
+# Log the values into the tabl
+log_values(LOC_ID, temp_time, [temp_avg, hum_avg], VAL_TBLS, conn)
 
 conn.close()
 
