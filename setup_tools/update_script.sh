0524a0bb34126852221cff2b48271b0e40b7f268
Barret <barret.obrock@gmail.com>
2019-09-07 19:02:18 -0500

    Remove sudo from pip install

--- setup_tools/update_script.sh
+++ setup_tools/update_script.sh
@@ -20,6 +20,6 @@ echo "Pulling update from git repo"
 echo "Beginning update of python package"
 #(cd ${KAVDIR} &&
 # TODO check if installed, then upgrade if so
-sudo pip3 install git+https://github.com/barretobrock/kavalkilu.git#egg=kavalkilu --upgrade
+pip3 install git+https://github.com/barretobrock/kavalkilu.git#egg=kavalkilu --upgrade
 
 printf "===============\nProcess completed\n"
