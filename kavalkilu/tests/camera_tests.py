7e15990a130ecc472ec800b6a4471c2dea877678
Barret <barret.obrock@gmail.com>
2019-09-28 10:50:51 -0500

    Add first round of test routines

---
+++ kavalkilu/tests/camera_tests.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import pytest
+from kavalkilu import Hosts, Keys
+from ..tools.camera import Amcrest, SecCamGroup
+
+
+class TestAmcrest:
+    creds = Keys().get_key('webcam_api')
+    known_ip = Hosts().get_host(name='ac_elutuba')['ip']
+    unknown_ip = '192.168.3.234'
+
+    def test_known_init(self):
+        cam = Amcrest(self.known_ip, self.creds)
+        assert cam is not None
+
+    def test_unknown_init(self):
+        cam = Amcrest(self.unknown_ip, self.creds)
+        assert cam is not None
+
+
+
---
+++ run_tests.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Simple command to run pytest routines
+pytest kavalkilu/tests/*
