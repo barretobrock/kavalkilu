20317e1895cbaa84eacc109c2e896fff1b68cb37
Barret <barret.obrock@gmail.com>
2019-09-28 19:46:49 -0500

    Bash syntax fix to check file, not dir

--- setup_tools/update_script.sh
+++ setup_tools/update_script.sh
@@ -26,7 +26,7 @@ pip3 install git+https://github.com/barretobrock/kavalkilu.git#egg=kavalkilu --u
 echo "Checking for crontab updates"
 MACHINE_HOSTNAME=${HOSTNAME}
 CRON_FILE=${KAVDIR}/documentation/crons/${MACHINE_HOSTNAME}.sh
-if [[ -f ${CRONFILE} ]];
+if [[ -f ${CRON_FILE} ]]
 then
     # Matching file found. Apply as update to cronjobs
     echo "Found matching cronjob file for ${CRON_FILE}. Applying changes..."
