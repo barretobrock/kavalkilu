f46f8966a06bb366ac90d37f29bc6fea91963a1d
Barret <barret.obrock@gmail.com>
2019-09-29 13:08:15 -0500

    Change reboot method in cron job files

--- documentation/crons/su-homeserv.sh
+++ documentation/crons/su-homeserv.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # MONTHLY REBOOT 15TH @23.57
-57 23 15 * * root shutdown -r now
+57 23 15 * * /sbin/shutdown -r now
--- documentation/crons/su-pi-porch.sh
+++ documentation/crons/su-pi-porch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # NIGHTLY REBOOT @23.57
-57 23 * * * root shutdown -r now
+57 23 * * * /sbin/shutdown -r now
