3408be433310d178b2e644bdeb7198452bd3465b
Barret <barret.obrock@gmail.com>
2019-10-20 20:35:28 -0500

    Change log reader cron job to run every 4 hours

--- documentation/crons/pi-garage.sh
+++ documentation/crons/pi-garage.sh
@@ -5,8 +5,8 @@ SENSORS=kavalkilu/sensors
 PY3=/usr/bin/python3
 
 # LOG ANALYSIS
-13 5 * * *      $PY3    $HOME/$SENSORS/log_reader.py
-32 3 20 * *     $PY3    $HOME/$SENSORS/log_remover.py debug
+0 */4 * * *     $PY3    $HOME/$SENSORS/log_reader.py
+32 3 20 * *     $PY3    $HOME/$SENSORS/log_remover.py -lvl debug
 # SYS DATA COLLECTION
 */10 * * * *    $PY3    $HOME/$SENSORS/net/machine_uptime.py
 # ENV DATA COLLECTION
