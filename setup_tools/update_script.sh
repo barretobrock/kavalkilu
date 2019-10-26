004ea01b6313af4a7db5dbc00e0ea7b593d35e7a
Barret <barret.obrock@gmail.com>
2019-10-26 09:43:10 -0500

    Improve repo installation process with no dependency flag (helps to speed installation on raspi when dependencies haven't changed

--- setup_tools/update_script.sh
+++ setup_tools/update_script.sh
@@ -1,44 +1,52 @@
 #!/usr/bin/env bash
 # Updates both the python package and the local git repo
 
+# ARGS
+INSTALL_SPEED=${1:-full}    # Full, won't skip dependencies on pip install, fast will
+
+NODEPS_FLAG=''
+if [[ "${INSTALL_SPEED}" == "fast" ]];
+then
+    echo "Not pip installing dependencies"
+    NODEPS_FLAG="--no-deps"
+fi
+
+# DIRECTORY SETUP
+# CD to the location of the package and pull from master
+REPO=kavalkilu
+PROJDIR=${HOME}/kavalkilu
+LT_PROJDIR=${HOME}/projects/kavalkilu
+
+if [[ -d ${LT_PROJDIR} ]]; then
+    # Laptop
+    PROJDIR=${LT_PROJDIR}
+fi
+
 # SETUP
 # --------------
 # FILE PATHS
-FUNCTIONS=./setup_tools/general_functions.sh
+FUNCTIONS=${PROJDIR}/setup_tools/general_functions.sh
 
 # Import common functions
 . ${FUNCTIONS} --source-only
 
-# DIRECTORY SETUP
-# CD to the location of the package and pull from master
-# Could be ~/kavalkilu or ~/projects/kavalkilu
-KAVDIR=${HOME}/kavalkilu
-LT_KAVDIR=${HOME}/projects/kavalkilu
-
-if [[ -d ${LT_KAVDIR} ]]; then
-    # Laptop
-    KAVDIR=${LT_KAVDIR}
-fi
-
 # GIT PULL
 # --------------
 announce_section "Pulling update from git repo"
-# TODO see if I can check if master is up to date before issuing command. If it is, don't pull
-(cd ${KAVDIR} && git pull origin master)
+(cd ${PROJDIR} && git pull origin master)
 
 # PY PACKAGE UPDATE
 # --------------
 # Then update the python package locally
 announce_section "Beginning update of python package"
-# TODO check if installed, then upgrade if so
-python3 -m pip install git+https://github.com/barretobrock/kavalkilu.git#egg=kavalkilu --upgrade
+python3 -m pip install git+https://github.com/barretobrock/${REPO}.git#egg=${REPO} --upgrade ${NODEPS_FLAG}
 
 # CRON UPDATE
 # --------------
 # Apply cronjob changes, if any.
 announce_section "Checking for crontab updates"
 MACHINE_HOSTNAME=${HOSTNAME}
-CRON_DIR=${KAVDIR}/documentation/crons
+CRON_DIR=${PROJDIR}/documentation/crons
 CRON_FILE=${CRON_DIR}/${MACHINE_HOSTNAME}.sh
 SUDO_CRON_FILE=${CRON_DIR}/su-${MACHINE_HOSTNAME}.sh
 
