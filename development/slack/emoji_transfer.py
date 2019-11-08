40cfcd4fe99f4869e254392cb95369c432b8ed34
Barret <barret.obrock@gmail.com>
2019-11-08 15:06:01 -0600

    Update emoji transfer script

--- development/slack/emoji_transfer.py
+++ development/slack/emoji_transfer.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
-from kavalkilu import Keys
+from kavalkilu import Keys, Log, LogArgParser
 from slacktools import SlackTools
 
 
-moji_dir = os.path.join(os.path.expanduser('~'), *['Downloads', 'mojis'])
+log = Log('emoji.extraction', log_lvl=LogArgParser().loglvl)
+
+moji_dir = os.path.join(os.path.expanduser('~'), *['Downloads', 'emojis'])
+
+os.path.isdir(moji_dir)
 
 okr_name = Keys().get_key('okr-name')
 okr_cookie = Keys().get_key('slack-okrcookie')
@@ -13,20 +17,20 @@ okr_token = Keys().get_key('slack-okrtoken')
 too_name = Keys().get_key('too-name')
 too_token = Keys().get_key('slack-tootoken')
 
-okr = SlackTools(okr_name, okr_token, okr_cookie)
-too = SlackTools(too_name, too_token)
+okr = SlackTools(log, team=okr_name, xoxp_token=okr_token, cookie=okr_cookie)
+too = SlackTools(log, team=too_name, xoxp_token=too_token)
 
 # Make list of exact matches
 exact_emojis = None
 # list of fuzzy matches
-fuzzy = '.*().*'
-
+fuzzy = '.*(mood|cloud|skel|spook|pipe|chef|press|eyes|wtf|trig|poo|uwu|bi(^g)).*'
 # Collect from workspace
 collected = too.match_emojis(exact_emojis, fuzzy)
 
 # Check against preexisting
 emojis = okr.get_emojis()
 missing_emojis = {k: v for k, v in collected.items() if k not in emojis.keys()}
+log.debug('{} emojis matched.'.format(len(missing_emojis)))
 
 # Download
 too.download_emojis(missing_emojis, moji_dir)
