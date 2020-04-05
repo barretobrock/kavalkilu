de46ed91ec1a4317c94dcba7ffc36c3a4e092441
bobrock <barret.obrock@gmail.com>
2020-04-04 19:07:25 -0500

    Add YrNo & OWM weather API classes, enforce type hints in weather group

--- kavalkilu/tools/weather.py
+++ kavalkilu/tools/weather.py
@@ -1,12 +1,126 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import json
+from typing import List, Optional, Union
 from collections import OrderedDict
 from datetime import datetime as dtt
 from datetime import timedelta
 import pandas as pd
+from meteocalc import feels_like, Temp, dew_point
+from pyowm import OWM
+from pyowm.weatherapi25.forecast import Forecast
+from yr.libyr import Yr
 from urllib.request import urlopen
 from .net import Keys
+
+
+# Locations for OWM
+OWM_ATX = 'Austin,US'
+OWM_TLL = 'Tallinn,EE'
+OWM_RKV = 'Rakvere,EE'
+
+# Some common locations for YrNo
+YRNO_LOC_ATX = 'USA/Texas/Austin'
+YRNO_LOC_TLL = 'Estonia/Harjumaa/Tallinn'
+YRNO_LOC_RKV = 'Estonia/Lääne-Virumaa/Rakvere'
+
+# NWS zones for alerts
+NWS_ZONES_ATX = ['TXC453', 'TXZ192']
+
+
+class OpenWeather:
+    def __init__(self, location: str, tz: str = 'US/Central'):
+        self.api_key = Keys().get_key('openweather_api')
+        self.owm = OWM(self.api_key)
+        self.location = location
+        self.tz = tz
+
+    def three_hour_summary(self) -> pd.DataFrame:
+        """3h summary for the next 5 days"""
+        data = self.owm.three_hours_forecast(self.location).get_forecast()
+        return self._process_data(data, self.tz)
+
+    def daily_summary(self) -> pd.DataFrame:
+        """daily forecast for the next 5 days"""
+        data = self.owm.daily_forecast(self.location, limit=5).get_forecast()
+        return self._process_data(data, self.tz)
+
+    @staticmethod
+    def _process_data(data: Union[Forecast], tz: str) -> pd.DataFrame:
+        cleaned = []
+        for pt in data:
+            temp = pt.get_temperature('celsius')['temp']
+            wind = pt.get_wind('meters_sec')['speed']
+            hum = pt.get_humidity()
+            feels = feels_like(Temp(temp, 'c'), hum, wind).c
+            dew_pt = pt.get_dewpoint() if pt.get_dewpoint() is not None else dew_point(temp, hum).c
+            pt_dict = {
+                'time': pd.to_datetime(pt.get_reference_time('iso')).tz_convert(tz),
+                'summary': pt.get_detailed_status(),
+                'precipIntensity': pt.get_rain()['3h'],
+                'temperature': temp,
+                'apparentTemperature': feels,
+                'dewPoint': dew_pt,
+                'humidity': hum / 100,
+                'pressure': pt.get_pressure()['press'],
+                'windSpeed': wind,
+                'windBearing': pt.get_wind('meters_sec')['deg'],
+                'cloudCover': pt.get_clouds() / 100,
+                'visibility': pt.get_visibility_distance()
+            }
+            cleaned.append(pt_dict)
+        return pd.DataFrame(cleaned)
+
+
+class YrNoWeather:
+    """Pulls weather data using Yr.no weather API"""
+    def __init__(self, location: str, tz: str = 'US/Central'):
+        """
+        Args:
+            location(str): location name `country/region/city`
+            tz(str):  time zone to record time
+        """
+        self.location = location
+        self.tz = tz
+
+    @staticmethod
+    def _process_data(data: dict) -> dict:
+        """Process the raw data from YrNo API"""
+        return {
+            'from': pd.to_datetime(data['@from']),
+            'to': pd.to_datetime(data['@to']),
+            'summary': data['symbol']['@name'],
+            'precipIntensity': float(data['precipitation']['@value']),
+            'windBearing': float(data['windDirection']['@deg']),
+            'windSpeed': float(data['windSpeed']['@mps']),
+            'windSummary': data['windSpeed']['@name'],
+            'temperature': float(data['temperature']['@value']),
+            'pressure': float(data['pressure']['@value'])
+        }
+
+    def current_summary(self) -> pd.DataFrame:
+        """Collect the current weather data for the location"""
+        data = Yr(location_name=self.location).now()
+        cleaned = pd.DataFrame(self._process_data(data), index=[1])
+        return cleaned
+
+    def hourly_summary(self) -> pd.DataFrame:
+        """Creates a 48 hour forecast summary"""
+        data = Yr(location_name=self.location, forecast_link='forecast_hour_by_hour')
+        return pd.DataFrame([self._process_data(x) for x in data.forecast()])
+
+    def daily_summary(self) -> pd.DataFrame:
+        """Creates a 7-day forecast summary"""
+        data = Yr(location_name=self.location)
+        df = pd.DataFrame([self._process_data(x) for x in data.forecast()])
+        # We'll need to group by day and have high/low calculated for each metric
+        keep_cols = ['from', 'precipIntensity', 'windSpeed', 'temperature', 'pressure']
+        df = df[keep_cols].groupby(pd.Grouper(key='from', freq='D')).agg(
+            {x: ['min', 'max'] for x in keep_cols[1:]}
+        )
+        # Flatten the columns for the dataframe, but keep everything preserved from each level
+        df.columns = ['_'.join(col).strip() for col in df.columns.values]
+        return df
 
 
 class DarkSkyWeather:
@@ -85,9 +199,9 @@ class DarkSkyWeather:
         return df
 
 
-class YrNoWeather:
+class YrNoSelenium:
     """
-    Collect data from YRNO
+    Collect data from YRNO via Selenium
     """
     def __init__(self):
         # Import selenium dependencies
@@ -113,26 +227,26 @@ class YrNoWeather:
             ('Võru', 'Estonia/Võrumaa/Võru')
         ))
 
-    def hourly_forecast(self, city):
+    def hourly_forecast(self, city: str) -> Optional[List[dict]]:
         weather_list = []
         city_path = self.cities.get(city)
         if city_path is None:
             return None
-        city_url = "http://www.yr.no/place/{}/hour_by_hour_detailed.html".format(city_path)
+        city_url = f"http://www.yr.no/place/{city_path}/hour_by_hour_detailed.html"
         self.ba.get(city_url)
         self.ba.fast_wait()
         # go through maximum four tables (there are usually just three)
         tbl_xpath = ''
         for x in range(1, 5):
             # xpath uses base 1 indexing!
-            tbl_xpath = '//table[@id="detaljert-tabell"][{}]'.format(x)
-            date_text = self.ba.get_elem(tbl_xpath + '/caption[strong]')
+            tbl_xpath = f'//table[@id="detaljert-tabell"][{x}]'
+            date_text = self.ba.get_elem(f'{tbl_xpath}/caption[strong]')
             date = dtt.strptime(date_text.text.replace('Detailed forecast ', ''), '%B %d, %Y').date()
             if date.day == self.tomorrow.day:
                 # found tomorrow's weather data
                 break
         if date.day > 0:
-            rows = self.ba.get_elem(tbl_xpath + '/tbody/tr', single=False)
+            rows = self.ba.get_elem(f'{tbl_xpath}/tbody/tr', single=False)
             for row in rows:
                 hour = (dtt.strptime(row.find_element_by_xpath('th').text, '%H:%M') - timedelta(hours=1)).time()
                 cols = row.find_elements_by_xpath('td')
@@ -177,11 +291,3 @@ class YrNoWeather:
                         weather_list_fix.append(fix_dict)
                 weather_list = weather_list_fix
         return weather_list
-
-
-
-
-
-
-
-
