c73073f69461f0522ebf1c531170f1d7d7f4e342
bobrock <barret.obrock@gmail.com>
2020-05-25 08:50:27 -0500

    Migrate crons

--- crons/lt-barrettp.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# VARIABLES
-SENSORS=kavalkilu/sensors
-PY3=/home/bobrock/venvs/kavkil/bin/python3
-
-# Vpulse Automation
-0 5 * * * export DISPLAY=:0; $PY3 $HOME/$SENSORS/vpulse/vpulse_auto.py
--- crons/su-pi-elutuba.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# NIGHTLY REBOOT @23.57
-57 23 * * * /sbin/shutdown -r now
