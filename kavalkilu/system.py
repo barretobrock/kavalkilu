874467ab730f440b55e47feea88cc11a48e20b89
Bobrock <barret.obrock@gmail.com>
2020-07-04 13:22:26 -0500

    Add system environment sensors to systools

--- kavalkilu/system.py
+++ kavalkilu/system.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import psutil
 from subprocess import check_output, CalledProcessError
-from typing import List, Union, Optional
+from typing import List, Union
 
 
 class SysTools:
@@ -11,6 +11,49 @@ class SysTools:
     def __init__(self):
         """Nothing to initialize, as these will just serve as a bundle"""
         pass
+
+    @staticmethod
+    def get_system_temps() -> dict:
+        """Get the temperature of various sensors on the machine """
+        sensor_temps = psutil.sensors_temperatures()
+        # Build a dictionary of average sensor temperatures
+        avg_temps = {}
+        for k, v in sensor_temps.items():
+            current_measurements = [x.current for x in v if x.current > 0]
+            if len(current_measurements) == 0:
+                continue
+            avg_measurement = sum(current_measurements) / len(current_measurements)
+            avg_temps[k] = avg_measurement
+        if len(avg_temps) > 0:
+            return avg_temps
+
+    @staticmethod
+    def get_cpu_percent() -> dict:
+        """Gets CPU usage percentage"""
+        cpu_use = psutil.cpu_percent()
+        return {'cpu-use': cpu_use}
+
+    @staticmethod
+    def get_mem_data():
+        """Gets memory usage data (in MBs)"""
+        ram = psutil.virtual_memory()
+        return {
+            'ram-total': ram.total / 2 ** 20,
+            'ram-used': ram.used / 2 ** 20,
+            'ram-free': ram.free / 2 ** 20,
+            'ram-percent_used': ram.percent / 100
+        }
+
+    @staticmethod
+    def get_disk_data():
+        """Gets disk usage data (in GBs)"""
+        disk = psutil.disk_usage('/')
+        return {
+            'disk-total': disk.total / 2 ** 30,
+            'disk-used': disk.used / 2 ** 30,
+            'disk-free': disk.free / 2 ** 30,
+            'disk-percent_used': disk.percent / 100
+        }
 
     @staticmethod
     def get_pids(process_name: str) -> Union[List, List[int]]:
