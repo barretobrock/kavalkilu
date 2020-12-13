b69abe24a1b5a9827cd4fcacf76d2101fd2ea9fc
Bobrock <barret.obrock@gmail.com>
2020-12-12 22:01:40 -0600

    Apply fixes to update_script procedures

--- _auto_config.sh
+++ _auto_config.sh
@@ -1,10 +1,10 @@
 # **This is an automatically-constructed file. See config.py for the source.**
 
-REPO_NAME=kavalkilu
-REPO_DIR=/home/bobrock/kavalkilu
-GIT_URL=git+https://github.com/barretobrock/kavalkilu.git#egg=kavalkilu
-VENV_PATH=${HOME}/venvs/kavkil/bin/python3
+REPO_NAME="kavalkilu"
+REPO_DIR="/home/bobrock/kavalkilu"
+GIT_URL="git+https://github.com/barretobrock/kavalkilu.git#egg=kavalkilu"
+VENV_PATH="${HOME}/venvs/kavkil/bin/python3"
 DEP_LINKS=(
-	git+https://github.com/barretobrock/easylogger/tarball/master#egg=easylogger
+	"git+https://github.com/barretobrock/easylogger/tarball/master#egg=easylogger"
 )
-MAIN_BRANCH=master
+MAIN_BRANCH="master"
