5160fd0454eb403bb2de6697ed738fd81e951339
bobrock <barret@tarkmail.com>
2021-11-01 08:17:58 -0500

    Change default location of kavkil dir

--- _auto_config.sh
+++ _auto_config.sh
@@ -1,7 +1,7 @@
 # **This is an automatically-constructed file. See config.py for the source.**
 
 REPO_NAME="kavalkilu"
-REPO_DIR="/home/bobrock/kavalkilu"
+REPO_DIR="/home/bobrock/extras/kavalkilu"
 GIT_URL="git+https://github.com/barretobrock/kavalkilu.git#egg=kavalkilu"
 VENV_PATH="${HOME}/venvs/kavkil/bin/python3"
 DEP_LINKS=(
