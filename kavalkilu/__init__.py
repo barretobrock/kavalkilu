f8d16eed011cfb6b1b23dd5110720e8ff4490801
bobrock <barret@tarkmail.com>
2022-04-20 17:57:02 -0500

    Update to new package management structure

--- kavalkilu/__init__.py
+++ kavalkilu/__init__.py
@@ -3,15 +3,31 @@
 from .date import DateTools
 from .errors import format_exception
 from .homeassistant import HAHelper
-from .influx import InfluxDBLocal, InfluxDBHomeAuto, InfluxDBPiHole, InfluxDBTracker
+from .influx import (
+    InfluxDBLocal,
+    InfluxDBHomeAuto,
+    InfluxDBPiHole,
+    InfluxDBTracker
+)
 from .log import LogWithInflux
 from .mqtt import MQTTClient
-from .net import Hosts, HostsRetrievalException, Keys, KeyRetrievalException, NetTools
-from .path import Path, HOME_SERVER_HOSTNAME
+from .net import (
+    Hosts,
+    HostsRetrievalException,
+    Keys,
+    KeyRetrievalException,
+    NetTools
+)
+from .path import (
+    Path,
+    HOME_SERVER_HOSTNAME
+)
 from .sqllite import SQLLiteLocal
-from .system import SysTools, GracefulKiller
+from .system import (
+    SysTools,
+    GracefulKiller
+)
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-__update_date__ = get_versions()['date']
-del get_versions
+
+__version__ = '1.5.0'
+__update_date__ = '2022-04-08_12:34:46'
--- run_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Simple command to run pytest routines
-pytest kavalkilu/tests/*
